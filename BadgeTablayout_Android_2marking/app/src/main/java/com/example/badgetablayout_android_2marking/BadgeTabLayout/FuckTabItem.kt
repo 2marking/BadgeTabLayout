32c9d0ee8901be6f37c05966823c9427dba59ab2
이교원 <cjswoakzld@naver.com>
2020-05-13 21:09:29 +0900

    Add to BadgeTablaout Custom xml, kotlin code

---
+++ BadgeTablayout_Android_2marking/app/src/main/java/com/example/badgetablayout_android_2marking/BadgeTabLayout/FuckTabItem.kt
@@ -0,0 +1,23 @@
+package com.example.badgetablayout_android_2marking.BadgeTabLayout
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.View
+import com.example.badgetablayout_android_2marking.R
+
+class FuckTabItem(ctx: Context, attrs: AttributeSet? = null) : View(ctx, attrs) {
+
+    var text:CharSequence? = null
+    var icon:Drawable? = null
+
+    init {
+        attrs?.let {
+            val a = ctx.obtainStyledAttributes(attrs, R.styleable.FuckTabItem)
+            text = a.getText(R.styleable.FuckTabItem_android_text)
+            icon = a.getDrawable(R.styleable.FuckTabItem_android_icon)
+            a.recycle()
+        }
+    }
+
+}
---
+++ BadgeTablayout_Android_2marking/app/src/main/java/com/example/badgetablayout_android_2marking/BadgeTabLayout/OnTabSelectedListener.kt
@@ -0,0 +1,11 @@
+package com.example.badgetablayout_android_2marking.BadgeTabLayout
+
+interface OnTabSelectedListener {
+
+    fun onTabSelected(tab: FuckTab)
+
+    fun onTabUnselected(tab: FuckTab)
+
+    fun onTabReselected(tab: FuckTab)
+
+}
